65b349786722d0ab930ba3627fb40ce044d9b240
Remy Sharp <remy@remysharp.com>
2020-10-22 12:38:00 +0100

    css tweak

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // DO NOT EDIT - this file is generated
 
 const API = "http://localhost:3100";
-const VERSION = "1.8.0";
+const VERSION = "1.9.0";
--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 
 // we'll version our cache (and learn how to delete caches in
 // some other post)
-const cacheName = 'v4.0';
+const cacheName = 'v4.1';
 
 self.addEventListener('install', (e) => {
   // once the SW is installed, go ahead and fetch the resources
